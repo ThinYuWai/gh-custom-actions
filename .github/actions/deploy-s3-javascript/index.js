a8b5c7447ebd2d652fa4eb84efad20440be2b115
ThinYuWai <thinyu354@gmail.com>
2024-07-23 01:23:58 +0900

    Deploy to S3

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -3,6 +3,15 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // 1) Get some input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2) Upload Files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${bucket} --region ${bucketRegion}`);
+
   core.notice('Hello From My Custom JavaScript Action!');
 }
 
