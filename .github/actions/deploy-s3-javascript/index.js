4206007c68322afe39e61ae7ef510ce58e048b2a
ThinYuWai <thinyu354@gmail.com>
2024-07-23 01:47:37 +0900

    Fixed

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -12,6 +12,9 @@ function run() {
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${bucket} --region ${bucketRegion}`);
 
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+
+  core.setOutput('website-url', websiteUrl); // ::set-output
   core.notice('Hello From My Custom JavaScript Action!');
 }
 
